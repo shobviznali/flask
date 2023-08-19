4762d7d4f40b9c2fa170badae31dbac8398447a8
ashoteghiazaryanashot@gmail.com <ashoteghiazaryanashot@gmail.com>
2023-08-19 14:33:30 +0400

    Updating

--- app/routes.py
+++ app/routes.py
@@ -7,6 +7,27 @@ from app import connection
 def home():
     return render_template("index.html", content="Home page")
 
+
+@app.route('/edit', methods=['GET', 'POST'])
+def edit():
+    if request.method == "POST":
+        username = request.form['username']
+        password = request.form['password']
+        new_password = request.form['new_password']
+        with connection.cursor() as cursor:
+            cursor.execute(f"""SELECT password FROM users WHERE username = %s""", (username,))
+            hash = cursor.fetchone()
+
+            update_query = "UPDATE users SET password = %s WHERE username = %s"
+
+            if not check_password_hash(hash[0], password):
+                return "Wrong password"
+            else:
+                hashed_password = generate_password_hash(new_password, method='sha256')
+                cursor.execute(update_query, (hashed_password, username))
+                return "Password updated"
+    return render_template('edit.html')
+
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     if request.method == "POST":
@@ -37,6 +58,23 @@ def register():
             connection.commit()
         return "User registered successfully"
     return render_template('register.html')
+
+
+
+@app.route('/delete', methods=['GET', 'POST'])
+def register():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        hashed_password = generate_password_hash(password, method='sha256')
+        insert_query = "INSERT INTO users (username, password) VALUES (%s, %s);"
+        with connection.cursor() as cursor:
+            cursor.execute(insert_query, (username, hashed_password))
+            connection.commit()
+        return "User registered successfully"
+    return render_template('register.html')
+
+
 
 @app.route('/about')
 def about():
